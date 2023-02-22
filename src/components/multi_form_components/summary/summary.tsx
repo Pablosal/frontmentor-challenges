7379b85ec2f08450bd37eb2cbbe033eaa489c04e
Pablo <pablosalgadou@gmail.com>
2023-02-22 09:38:01 -0300

    save

--- src/components/multi_form_components/summary/summary.tsx
+++ src/components/multi_form_components/summary/summary.tsx
@@ -1,12 +1,20 @@
+import './summary.sass';
 const Summary = () => {
   return (
-    <article className="summary_container">
-      <main>
-
-        <div>
-          
+    <article className="summary__container">
+      <main className="summary__main-content">
+        <div className="summary__main-displayer">
+          <div className="summary_main-description">
+            <span>Arcade (Monthly)</span>
+            <span>Change</span>
+          </div>
+          <span className="summary__main-price">$9/mo</span>
         </div>
-        <div></div>
+        <hr />
+        <div>
+          <div className="summary__first-description"></div>
+          <div className="summary__second-description"></div>
+        </div>
       </main>
       <footer>
         <span>Total (per year)</span>
